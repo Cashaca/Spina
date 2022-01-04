ab62018fa61bd51c38b9768d113e75c22ce12494
Peter Loomans <loomans.peter@gmail.com>
2022-01-04 18:48:41 -0300

    filter resources with current_account in pages index view

--- db/migrate/17_add_account_relation_to_pages_resources_navigations.rb
+++ db/migrate/17_add_account_relation_to_pages_resources_navigations.rb
@@ -3,5 +3,11 @@ class AddAccountRelationToPagesResourcesNavigations < ActiveRecord::Migration[6.
     add_reference :spina_pages, :account, index: true
     add_reference :spina_resources, :account, index: true
     add_reference :spina_navigations, :account, index: true
+    account = Spina::Account.first
+    if account
+      Spina::Page.where(account_id:nil).update_all(account_id:account.id)
+      Spina::Resource.where(account_id:nil).update_all(account_id:account.id)
+      Spina::Navigation.where(account_id:nil).update_all(account_id:account.id)
+    end
   end
 end
